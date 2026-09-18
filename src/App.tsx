f479bf624ac9a01e1f67911a4d1db26f50ef5eb6
JaradSpencer <audiospencer@gmail.com>
2026-09-18 12:52:05 -0500

    template2

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>My Todo List</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
